3014dd5286b0e67aded804d90e3a85ce2c37a658
mahian ahmad <mahianahamd9@gmail.com>
2022-11-08 13:31:07 +0600

    get services data

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
+const port = process.env.PORT || 5000;
 const app = express();
 
+
+const uri = "mongodb://localhost:27017";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try{
+        const userCollection = client.db('shift-lawyer').collection('services');
+
+        app.get('/services', async (req, res)=>{
+            const query = {};
+            const cursor = userCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+    }
+    finally{}
+}
+run().catch(err => console.log(err));
+
+
+app.get('/', (req, res)=>{
+    res.send('hellow')
+})
+
+app.listen(port);
