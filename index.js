77ef4551a01eda32bdf4fe59c9056439d015eb3a
mahian ahmad <mahianahamd9@gmail.com>
2022-11-09 01:44:23 +0600

    fix single data issue

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ async function run() {
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
-            const service = serviceCollection.findOne(query);
+            const service = await serviceCollection.findOne(query);
             res.send(service);
         });
 
