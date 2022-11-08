b01a5085315c5a6879659ea990ecfc45c7282fd4
mahian ahmad <mahianahamd9@gmail.com>
2022-11-09 02:38:56 +0600

    delete data

--- index.js
+++ index.js
@@ -36,6 +36,13 @@ async function run() {
             const service = req.body;
             const result = await serviceCollection.insertOne(service);
             res.send(result);
+        });
+
+        app.delete('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const service = await serviceCollection.deleteOne(query);
+            res.send(service)
         })
     }
     finally { }
