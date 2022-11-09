ab7c7a091e738c2f4cd3f9430192b3ec017b7bb4
mahian ahmad <mahianahamd9@gmail.com>
2022-11-09 17:06:47 +0600

    get limited services

--- index.js
+++ index.js
@@ -25,6 +25,13 @@ async function run() {
             res.send(services);
         });
 
+        app.get('/limitedServices', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.limit(3).toArray();
+            res.send(services);
+        });
+
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
