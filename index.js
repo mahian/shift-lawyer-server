9a0b745f25349889490b04b4ce1b1a3d381cac12
mahian ahmad <mahianahamd9@gmail.com>
2022-11-10 14:25:27 +0600

    add review data

--- index.js
+++ index.js
@@ -17,12 +17,30 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const serviceCollection = client.db('shift-lawyer').collection('services');
+        const reviewCollection = client.db('shift-lawyer').collection('reviews');
 
+
+        // get data
         app.get('/services', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        });
+
+        app.get('/reviews', async (req, res) => {
+            const query = {};
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
+
+        app.get('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {serviceId : id};
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
         });
 
         app.get('/limitedServices', async (req, res) => {
@@ -39,24 +57,34 @@ async function run() {
             res.send(service);
         });
 
+        // post data
         app.post('/add-service', async (req, res) => {
             const service = req.body;
             const result = await serviceCollection.insertOne(service);
             res.send(result);
         });
 
+        app.post('/add-review', async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
+        })
+
+
+        // delete data
         app.delete('/services/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)}
+            const query = { _id: ObjectId(id) }
             const service = await serviceCollection.deleteOne(query);
             res.send(service)
         })
 
+        // update/put data
         app.put('/services/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const service = req.body;
-            const option = {upsert: true};
+            const option = { upsert: true };
             const updatedService = {
                 $set: {
                     title: service.title,
